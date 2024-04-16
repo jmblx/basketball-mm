419acd00d3ef84cbd5d65460eeeedbc11ffef6fb
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2024-04-16 23:35:39 +0300

    s

--- backend/docker/app.sh
+++ backend/docker/app.sh
@@ -10,4 +10,4 @@ alembic upgrade head
 
 cd src
 
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 -
